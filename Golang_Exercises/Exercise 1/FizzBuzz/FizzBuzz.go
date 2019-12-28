400f8b5351b6a6648060ceaaa503a809ebb52ddf
mulugeta <mulugetaewnu1@.com>
2019-12-28 13:51:19 +0300

    multiple of 3 and 5

--- Golang_Exercises/Exercise 1/FizzBuzz/FizzBuzz.go
+++ Golang_Exercises/Exercise 1/FizzBuzz/FizzBuzz.go
@@ -1,6 +1,16 @@
 package main
 import "fmt"
 
+
 func main() {
+for index := 1; index < 100; index++ {
+	if  index % 5== 0 && index % 3== 0{
+		fmt.Println("Fizz Buzz",index)
+	}else if index % 3== 0 {
+		fmt.Println("Fizz",index)
+	}else if  index % 5== 0 {
+		fmt.Println("Buzz",index)
+	}
+}
 
 }
