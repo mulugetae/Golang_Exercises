2391057b5e63b1c229eee36758b7311f8994a2ad
mulugeta <mulugetaewnu1@.com>
2019-12-29 15:38:56 +0300

    max and min

---
+++ Golang_Exercises/Exercise 2/Minimum and maximum/Minimum.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func Min(slice []int) int {
+	min := slice[4]
+	for index := 1; index < len(slice); index++ {
+		if (min == index){
+			break
+		}else{
+			if min > slice[index] {
+				min = slice[index]
+			}
+		}
+		
+	}
+	return min
+}
+
+func main() {
+	slice := []int{6, 15, 3, 7, 10}
+	fmt.Println(Min(slice))
+
+}
